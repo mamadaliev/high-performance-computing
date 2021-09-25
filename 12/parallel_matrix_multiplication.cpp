7b6ce92a1178631e891ea53946604ee36344da71
Sherzod Mamadaliev <mamadaliev@yahoo.com>
2021-09-25 21:59:18 +0300

    Fix parallel matrix multiplication algorithm

--- 12/parallel_matrix_multiplication.cpp
+++ 12/parallel_matrix_multiplication.cpp
@@ -11,18 +11,18 @@ void init_matrix(int n1, int n2, ifstream &in, vector<vector<int>> &A);
 void out_matrix(int n1, int n2, const vector<vector<int>> &A);
 
 // структуры для обмена между процессами
-struct Item {
+typedef struct Item {
     int value1; // значение 1
     int value2; // значение 2
     int x;      // позиция x
     int y;      // позиция y
-};
+} matrix_item;
 
-struct Result {
+typedef struct Result {
     int value; // значение 1
     int x;     // позиция x
     int y;     // позиция y
-};
+} calc_result;
 
 int main(int argc, char **argv) {
     int n1 = 3, n2 = 4, n3 = 3;
@@ -36,16 +36,45 @@ int main(int argc, char **argv) {
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
+    // Опеределение новых MPI типов
+    const int item_type_fields_size = 4;
+    int item_type_block_size[4] = {50, 50, 50, 50};
+    MPI_Datatype item_subtypes[4] = {MPI_INT, MPI_INT, MPI_INT, MPI_INT};
+    MPI_Datatype mpi_item_type;
+    MPI_Aint item_type_offsets[4];
+    item_type_offsets[0] = offsetof(Item, value1);
+    item_type_offsets[1] = offsetof(Item, value2);
+    item_type_offsets[2] = offsetof(Item, x);
+    item_type_offsets[3] = offsetof(Item, y);
+
+    const int result_type_fields_size = 3;
+    int result_type_block_size[4] = {50, 50, 50};
+    MPI_Datatype result_subtypes[3] = {MPI_INT, MPI_INT, MPI_INT};
+    MPI_Datatype mpi_result_type;
+    MPI_Aint result_type_offsets[3];
+    result_type_offsets[0] = offsetof(Result, value);
+    result_type_offsets[1] = offsetof(Result, x);
+    result_type_offsets[2] = offsetof(Result, y);
+
+    MPI_Type_create_struct(item_type_fields_size, item_type_block_size, item_type_offsets, item_subtypes, &mpi_item_type);
+    MPI_Type_create_struct(result_type_fields_size, result_type_block_size, result_type_offsets, result_subtypes, &mpi_result_type);
+    MPI_Type_commit(&mpi_item_type);
+    MPI_Type_commit(&mpi_result_type);
+
     printf("\nProcess %d of %d is running...\n", rank, size);
 
+    // Счетчик вызовов
+    vector<int> process_call_count(size);
+
     // Переменные для обмена между процессами
-    Item item {};
-    Result result {};
+    Item item{};
+    Result result{};
+
+    vector<vector<int>> A(n1, vector<int>(n2));
+    vector<vector<int>> B(n2, vector<int>(n3));
 
     if (rank == 0) {
         std::ifstream in("in.txt");
-        vector<vector<int>> A(n1, vector<int>(n2));
-        vector<vector<int>> B(n2, vector<int>(n3));
 
         init_matrix(n1, n2, in, A);
         init_matrix(n2, n3, in, B);
@@ -63,35 +92,49 @@ int main(int argc, char **argv) {
 
         printf("\n");
         printf("A * B = C\n");
+    }
 
-        int process_index = 1;
+    int process_index = 1;
 
-        for (int i = 0; i < n1; ++i) {
-            for (int j = 0; j < n3; ++j) {
-                for (int k = 0; k < n2; ++k) {
-                    // C[i][j] += A[i][k] * B[k][j];
-                    item.value1 = A[i][k];
-                    item.value2 = B[k][j];
-                    item.x = i;
-                    item.y = j;
+    for (int i = 0; i < n1; ++i) {
+        for (int j = 0; j < n3; ++j) {
+            for (int k = 0; k < n2; ++k) {
+                // C[i][j] += A[i][k] * B[k][j];
+                item.value1 = A[i][k];
+                item.value2 = B[k][j];
+                item.x = i;
+                item.y = j;
 
-                    if (process_index == size) {
-                        process_index = 1;
-                    }
-                    MPI_Send(&item, sizeof(item), MPI_CHAR, process_index, 5, MPI_COMM_WORLD);
-                    MPI_Recv(&result, sizeof(result), MPI_CHAR, process_index, 5, MPI_COMM_WORLD, &status);
-                    C[result.x][result.y] = result.value;
-                    process_index++;
+                if (process_index == size) {
+                    process_index = 1;
                 }
+
+                if (rank == 0) {
+                    MPI_Send(&item, 1, mpi_item_type, process_index, 5, MPI_COMM_WORLD);
+                    printf("P%d: Sent item from P%d to P%d.\n", 0, 0, process_index);
+                }
+
+                if (rank == process_index) {
+                    MPI_Recv(&item, 1, mpi_item_type, 0, 5, MPI_COMM_WORLD, &status);
+                    printf("P%d: Received result from P%d to P%d. Items = [%d, %d]\n",
+                           process_index, 0, process_index, item.value1, item.value2);
+                    int c = item.value1 * item.value2;
+                    result.value = c;
+                    result.x = item.x;
+                    result.y = item.y;
+                    MPI_Send(&result, 1, mpi_result_type, 0, 5, MPI_COMM_WORLD);
+                    printf("P%d: Sent item from P%d to P%d.\n", process_index, process_index, 0);
+                }
+
+                if (rank == 0) {
+                    MPI_Recv(&result, 1, mpi_result_type, process_index, 5, MPI_COMM_WORLD, &status);
+                    printf("P%d: Received result from P%d to P%d. Result = %d\n", 0, process_index, 0, result.value);
+                    C[result.x][result.y] = result.value;
+                    process_call_count[process_index]++;
+                }
+                process_index++;
             }
         }
-    } else {
-        MPI_Recv(&item, sizeof(item), MPI_CHAR, 0, 5, MPI_COMM_WORLD, &status);
-        int c = item.value1 * item.value2;
-        result.value = c;
-        result.x = item.x;
-        result.y = item.y;
-        MPI_Send(&result, sizeof(item), MPI_CHAR, 0, 5, MPI_COMM_WORLD);
     }
 
     if (rank == 0) {
@@ -109,6 +152,7 @@ int main(int argc, char **argv) {
             out << "\n";
         }
     }
+    MPI_Finalize();
     return 0;
 }
 
