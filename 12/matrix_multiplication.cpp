0b15237307d4985e1c366cb945e7843f77f06a77
Sherzod Mamadaliev <mamadaliev@yahoo.com>
2021-09-30 12:25:03 +0300

    Reformat code and fix display of results

--- 12/matrix_multiplication.cpp
+++ 12/matrix_multiplication.cpp
@@ -5,12 +5,13 @@
 
 using namespace std;
 
-void init_matrix(int n1, int n2, ifstream &in, vector<vector<int>> &A);
+void init_matrix(int n1, int n2, ifstream &in, vector<vector<double>> &A);
 
-void out_matrix(int n1, int n2, const vector<vector<int>> &A);
+void out_matrix(int n1, int n2, const vector<vector<double>> &A);
 
-vector<vector<int>> &
-multiply(int n1, int n2, int n3, const vector<vector<int>> &A, const vector<vector<int>> &B, vector<vector<int>> &C);
+vector<vector<double>> &
+multiplication(int n1, int n2, int n3, const vector<vector<double>> &A, const vector<vector<double>> &B,
+               vector<vector<double>> &C);
 
 int main() {
     int n1 = 3, n2 = 4, n3 = 3;
@@ -18,13 +19,13 @@ int main() {
     std::ifstream in("in.txt");
     std::ofstream out("out.txt");
 
-    vector<vector<int>> A(n1, vector<int>(n2));
-    vector<vector<int>> B(n2, vector<int>(n3));
+    vector<vector<double>> A(n1, vector<double>(n2));
+    vector<vector<double>> B(n2, vector<double>(n3));
 
     c_x_size = n1 < n2 ? n1 : n2;
     c_y_size = n2 < n3 ? n2 : n3;
 
-    vector<vector<int>> C(c_x_size, vector<int>(c_y_size));
+    vector<vector<double>> C(c_x_size, vector<double>(c_y_size));
 
     init_matrix(n1, n2, in, A);
     init_matrix(n2, n3, in, B);
@@ -43,7 +44,7 @@ int main() {
     printf("\n");
     printf("A * B = C\n");
 
-    C = multiply(n1, n2, n3, A, B, C);
+    C = multiplication(n1, n2, n3, A, B, C);
 
     printf("\n");
     printf("Matrix C:\n");
@@ -60,8 +61,9 @@ int main() {
     return 0;
 }
 
-vector<vector<int>> &
-multiply(int n1, int n2, int n3, const vector<vector<int>> &A, const vector<vector<int>> &B, vector<vector<int>> &C) {
+vector<vector<double>> &
+multiplication(int n1, int n2, int n3, const vector<vector<double>> &A, const vector<vector<double>> &B,
+               vector<vector<double>> &C) {
     for (int i = 0; i < n1; ++i)
         for (int j = 0; j < n3; ++j)
             for (int k = 0; k < n2; ++k) {
@@ -70,16 +72,16 @@ multiply(int n1, int n2, int n3, const vector<vector<int>> &A, const vector<vect
     return C;
 }
 
-void out_matrix(int n1, int n2, const vector<vector<int>> &A) {
+void out_matrix(int n1, int n2, const vector<vector<double>> &A) {
     for (int i = 0; i < n1; ++i) {
         for (int j = 0; j < n2; ++j) {
-            printf("%d ", A[i][j]);
+            printf("%6.2f", A[i][j]);
         }
         printf("\n");
     }
 }
 
-void init_matrix(int n1, int n2, ifstream &in, vector<vector<int>> &A) {
+void init_matrix(int n1, int n2, ifstream &in, vector<vector<double>> &A) {
     for (int i = 0; i < n1; ++i) {
         for (int j = 0; j < n2; ++j) {
             in >> A[i][j];
